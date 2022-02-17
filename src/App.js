f2548727f35fcecb04fa783c245383e7f1ef513e
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-17 09:52:49 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App">
+        <img src={logo} className="App-logo"
+        alt="Lexicon Logo"/>
+
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
