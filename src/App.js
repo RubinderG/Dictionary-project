449a7c607ee523289c8478fa850218d399e2d0a9
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-17 13:43:00 +0000

    Footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App">
-        <img src={logo} className="App-logo" alt="Lexicon Logo" />
-      </header>
+      <div className="container">
+        <header className="App">
+          <img src={logo} className="App-logo" alt="Lexicon Logo" />
+        </header>
+        <footer className="text-center">
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/rubinder/"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            Rubinder {""}
+          </a>
+          and is {""}
+          <a
+            href="https://github.com/RubinderG/Dictionary-project"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
