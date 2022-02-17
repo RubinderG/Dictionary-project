4f978d8d2532987490087329c49ba2fb3885800b
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-17 13:23:11 +0000

    Logo Styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from "./logo.png";
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App">
-        <img src={logo} className="App-logo"
-        alt="Lexicon Logo"/>
-
+        <img src={logo} className="App-logo" alt="Lexicon Logo" />
       </header>
-      
     </div>
   );
 }
-
-export default App;
