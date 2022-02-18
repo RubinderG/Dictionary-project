c6c39e5c3d3b77b01c539b1b60bbd34518f253bf
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-18 11:12:56 +0000

    Rendered the word definitions

--- src/Lexicon.js
+++ src/Lexicon.js
@@ -3,19 +3,17 @@ import axios from "axios";
 import Results from "./Results";
 import "./Lexicon.css";
 
-export default function Lexicon() {
+export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState("{}");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    //console.log(response.data[0]);
     setResults(response.data[0]);
-    //console.log(response.data[0].meaning[0].defintions[0].definition);
   }
 
   function search(event) {
     event.preventDefault();
-
+    
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -29,7 +27,7 @@ export default function Lexicon() {
     <div className="Lexicon">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
-        <Results />
+        <Results results={results} />
       </form>
     </div>
   );
