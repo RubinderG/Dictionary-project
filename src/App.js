cb6d46191d811a0f0b467deab83ccc9c213c5f22
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-18 15:52:55 +0000

    Background styling

--- src/App.js
+++ src/App.js
@@ -4,33 +4,35 @@ import Lexicon from "./Lexicon.js";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App">
-          <img src={logo} className="App-logo" alt="Lexicon Logo" />
-        </header>
-        <main>
-          <Lexicon defaultKeyword="lexicon" />
-        </main>
-        <footer className="App-footer">
-          This project was coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/rubinder/"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            Rubinder {""}
-          </a>
-          and is {""}
-          <a
-            href="https://github.com/RubinderG/Dictionary-project"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
-        <br />
+    <div className="background">
+      <div className="App">
+        <div className="container">
+          <header className="App">
+            <img src={logo} className="App-logo" alt="Lexicon Logo" />
+          </header>
+          <main>
+            <Lexicon defaultKeyword="lexicon" />
+          </main>
+          <footer className="App-footer">
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/rubinder/"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              Rubinder {""}
+            </a>
+            and is {""}
+            <a
+              href="https://github.com/RubinderG/Dictionary-project"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </footer>
+          <br />
+        </div>
       </div>
     </div>
   );
